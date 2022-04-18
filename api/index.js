2c2b1a8f3ae655aa15e4e2f2c982fb2c33f901cf
david <dlonicholas@gmail.com>
2022-04-17 20:39:06 -0600

    dotenv setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
+require("dotenv").config({path: "../.env"});
+
 const app = express();
-const port = 8080;
+const port = process.env["API_PORT"] || 8080;
 
 app.get("/", (req, res) => {
 	res.send("Hello from api-server");
