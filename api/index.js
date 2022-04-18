8e092b0b265c9f1df26a2d5de4f5c4939942f76b
david <dlonicholas@gmail.com>
2022-04-17 20:56:07 -0600

    cors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
-
+var cors = require("cors");
 require("dotenv").config({path: "../.env"});
 
 const app = express();
+app.use(cors());
 const port = process.env["API_PORT"] || 8080;
 
 app.get("/", (req, res) => {
 	res.send("Hello from api-server");
 });
+
+var allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				var msg =
+					"The CORS policy for this site does not " +
+					"allow access from the specified Origin.";
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 app.listen(port, () => {
 	console.log(`api-server listening at http://localhost:${port}`);
