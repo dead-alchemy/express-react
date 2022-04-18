37e931c0b9859a2dc7efdc6b9ffef75f1267198b
david <dlonicholas@gmail.com>
2022-04-18 17:38:01 -0600

    typescript

--- client/src/service/service.ts
+++ client/src/service/service.ts
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const api_base_url = "http://localhost:8080";
 
-export const requestApi = async (sport) => {
-	return await axios.get(api_base_url).then(function (req) {
+export const requestApi = async () => {
+	return await axios.get(api_base_url).then(function (req: any) {
 		return req.data;
 	});
 };
