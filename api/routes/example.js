2060ded60e71ec6f424e21a74584cbe77b21c795
david <dlonicholas@gmail.com>
2022-07-20 21:10:21 -0600

    api bug fixing.

--- api/routes/example.js
+++ api/routes/example.js
@@ -3,11 +3,5 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = router.get("/", (req, res) => {
-	console.log("hello");
-	res.send("Hello from api-server");
-});
-
-module.exports = router.get("/api", (req, res) => {
-	console.log("hello");
 	res.send("Hello from api-server");
 });
