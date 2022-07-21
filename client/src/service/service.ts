809fa317491df8367758a7f422edbef7f3860314
david <dlonicholas@gmail.com>
2022-07-20 21:14:57 -0600

    react app js.

--- client/src/service/service.ts
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-const api_base_url = "http://localhost:8080/api";
-
-export const requestApi = async () => {
-	return await axios.get(api_base_url).then(function (req: any) {
-		return req.data;
-	});
-};
