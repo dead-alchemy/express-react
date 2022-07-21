603fd576ddb9779292ea91684fde248a28e9b20a
david <dlonicholas@gmail.com>
2022-07-20 21:47:45 -0600

    adding chakra and react-query

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./routes/App";
+import {ChakraProvider} from "@chakra-ui/react";
+
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 //import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	//<React.StrictMode>
-	<App />
+	<QueryClientProvider client={queryClient}>
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
+		<ReactQueryDevtools initialIsOpen={false} />
+	</QueryClientProvider>
 	//</React.StrictMode>
 );
 
