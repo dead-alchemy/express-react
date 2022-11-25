3b2a76642eccb7e123b0f9d61aadf5af6cb57db4
david <dlonicholas@gmail.com>
2022-11-25 15:46:16 -0700

    obj destructuring.

--- api/routes/postgress.js
+++ api/routes/postgress.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const {getTest} = require("../model/postgress");
 
 module.exports = router.get("/", async (req, res) => {
-	const data = await getTest();
-	if (data.status === 200) {
-		res.send(data);
+	const {status, data} = await getTest();
+	if (status === 200) {
+		res.send({data: data, rows: data.length});
 	} else {
 		// add some handling if we get an error back from our model.
 		res.status(500);
