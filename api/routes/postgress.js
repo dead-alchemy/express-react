268cbd2ce3c33c12024d8aa6b4ba0c99be43aeaf
david <dlonicholas@gmail.com>
2022-11-25 15:44:34 -0700

    more comment.

--- api/routes/postgress.js
+++ api/routes/postgress.js
@@ -9,6 +9,7 @@ module.exports = router.get("/", async (req, res) => {
 	if (data.status === 200) {
 		res.send(data);
 	} else {
+		// add some handling if we get an error back from our model.
 		res.status(500);
 		res.send("There was an error");
 	}
