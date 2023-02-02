1585f9e73933aed676de3aed66c685778e1de3f0
dead-alchemy <dlonicholas@gmail.com>
2023-02-01 21:14:56 -0700

    removignn console.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default ({mode}) => {
-	//console.log(import.meta.env.VITE_PORT);
-
 	const env = loadEnv(mode, "../");
 	return defineConfig({
 		envDir: "../",
